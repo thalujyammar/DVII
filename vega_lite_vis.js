d15d460905a7fcc29507c9392abc2ddd5be70c4d
thalujyammar <80653094+thalujyammar@users.noreply.github.com>
2022-10-19 01:56:37 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,24 @@
+var vg_1 = "x.vg.json";
+var vg_2 = "ps5.vg.json";
+var vg_3 = "nin.vg.json";
+var vg_4 = "steam.vg.json";
+
+vegaEmbed("#dv1", vg_1).then(function(result) {
+    ("https://vega.github.io/vega/docs/api/view/")
+
+}).catch(console.error);
+
+vegaEmbed("#dv2", vg_2).then(function(result) {
+    ("https://vega.github.io/vega/docs/api/view/")
+
+}).catch(console.error);
+
+vegaEmbed("#dv3", vg_3).then(function(result) {
+    ("https://vega.github.io/vega/docs/api/view/")
+
+}).catch(console.error);
+
+vegaEmbed("#dv4", vg_4).then(function(result) {
+    ("https://vega.github.io/vega/docs/api/view/")
+
+}).catch(console.error);
