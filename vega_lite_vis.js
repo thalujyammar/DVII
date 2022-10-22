35c7ff5d0266a2b741a5cf800ec42a77e9bdc971
thalujyammar <80653094+thalujyammar@users.noreply.github.com>
2022-10-22 22:55:27 +1100

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,6 +2,9 @@ var vg_1 = "x.vg.json";
 var vg_2 = "ps5.vg.json";
 var vg_3 = "nin.vg.json";
 var vg_4 = "steam.vg.json";
+var vg_5 = "final.vg.json"
+var vg_6 = "lollipop.vg.json"
+var vg_7 = "linechart.vg.json"
 
 vegaEmbed("#dv1", vg_1).then(function(result) {
     ("https://vega.github.io/vega/docs/api/view/")
@@ -22,3 +25,21 @@ vegaEmbed("#dv4", vg_4).then(function(result) {
     ("https://vega.github.io/vega/docs/api/view/")
 
 }).catch(console.error);
+
+vegaEmbed("#dv5", vg_5).then(function(result) {
+    ("https://vega.github.io/vega/docs/api/view/")
+
+}).catch(console.error);
+
+vegaEmbed("#dv6", vg_6).then(function(result) {
+    ("https://vega.github.io/vega/docs/api/view/")
+
+}).catch(console.error);
+
+vegaEmbed("#dv7", vg_7).then(function(result) {
+    ("https://vega.github.io/vega/docs/api/view/")
+
+}).catch(console.error);
+
+
+
